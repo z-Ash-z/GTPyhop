568e907e31514152f00d89cf1d6efd719573b619
Aneesh Chodisetty <ch.aneesh1996@gmail.com>
2023-04-14 04:34:44 -0400

    Added comments and fixed bugs

--- Examples/satellite_htn/methods_updated.py
+++ Examples/satellite_htn/methods_updated.py
@@ -6,6 +6,10 @@ Method definitions for satellite_htn
 import gtpyhop
 
 ################################################################################
+# Some global variables
+first_run = True
+
+################################################################################
 # The helper functions.
 
 def getPointingTasks(state, mgoal):
@@ -48,7 +52,10 @@ def getHaveImageTasks(state, mgoal):
     if 'have_image' in mgoal.__dict__.keys():
         if 'have_image' in state.__dict__.keys():
             for key in mgoal.have_image.keys():
-                if mgoal.have_image[key] != state.have_image[key]:
+                if key in state.have_image.keys():
+                    if mgoal.have_image[key] != state.have_image[key]:
+                        task_list.append(key)
+                else:
                     task_list.append(key)
         else:
             return mgoal.have_image.keys()
@@ -57,8 +64,22 @@ def getHaveImageTasks(state, mgoal):
 
 
 def getBestCalibTarget(state, instrument, satellite, calib_target):
+    """
+    Gets the best calibration target for the instrument in the satellite.
+
+    Args:
+        state: The state of the system.
+        instrument: The instrument that needs to be calibrated.
+        satellite: The satellite which houses the instrument.
+        calib_target: The calibration target that will be updated.
+
+    Returns:
+        The calibration target with the lowest slew_time.
+    """
     best_location_cost = float('inf')
     for target in state.calibration_target[instrument]:
+        if target == state.pointing[satellite]:
+            return target
         if state.slew_time[(state.pointing[satellite], target)] < best_location_cost:
             best_location_cost = state.slew_time[(state.pointing[satellite], target)]
             calib_target = target
@@ -66,7 +87,19 @@ def getBestCalibTarget(state, instrument, satellite, calib_target):
 
 
 def select_best_satellite(state, goal, new_direction, mode, pointing_tasks):
+    """
+    The best satellite needed for the pointing task.
 
+    Args:
+        state: The state of the system.
+        goal: The goal state.
+        new_direction: The direction which the satellite needs to face.
+        mode: The mode in which we need an image.
+        pointing_tasks: The number of pointing tasks, if any.
+
+    Returns:
+        A dictionary containing the instrument, satellite and calibration_target that needs to be part of the plan
+    """
     slew_times = {}
     slew_times[float('inf')] = {'instrument' : None,
                                 'satellite' : None,
@@ -82,6 +115,7 @@ def select_best_satellite(state, goal, new_direction, mode, pointing_tasks):
 
         satellite = state.on_board[instrument]
         
+        # If instrument is already calibrated, no need for additional slew_time.
         if 'calibrated' in state.__dict__.keys():
             if instrument in state.calibrated.keys():
                 if not state.calibrated[instrument]:
@@ -93,7 +127,8 @@ def select_best_satellite(state, goal, new_direction, mode, pointing_tasks):
 
         if need_calibration:
             calib_target = getBestCalibTarget(state, instrument, satellite, calib_target)
-            cost += state.slew_time[(state.pointing[satellite], calib_target)]
+            if calib_target != state.pointing[satellite]:
+                cost += state.slew_time[(state.pointing[satellite], calib_target)]
 
         
         if calib_target:
@@ -101,9 +136,12 @@ def select_best_satellite(state, goal, new_direction, mode, pointing_tasks):
         else:
             cost += state.slew_time[(state.pointing[satellite], new_direction)]
 
-        if satellite in pointing_tasks:
-            cost += state.slew_time[(goal.pointing[satellite], new_direction)]
+        # If there are pointing tasks check if we have enough fuel to complete them.
+        if pointing_tasks:
+            if satellite in pointing_tasks:
+                cost += state.slew_time[(goal.pointing[satellite], new_direction)]
 
+        # Reject satellite if we do not have enough fuel for all the tasks.
         if state.fuel[satellite] > cost:
             slew_times[cost] = {'instrument' : instrument,
                                 'satellite' : satellite,
@@ -113,20 +151,36 @@ def select_best_satellite(state, goal, new_direction, mode, pointing_tasks):
 
 
 def getImageStatus(state, mgoal, task, pointing_tasks):
-    
+    """
+    Method to compute the status of the image task.
+
+    Args:
+        state: The state of the system.
+        mgoal: The goal state.
+        task: The current task.
+        pointing_tasks: The list of pointing tasks.
+
+    Returns:
+        A dictionary containing the status, instrument, satellite and calibration_target that needs to be part of the plan
+    """
     new_direction, mode = task
 
-    status = select_best_satellite(state, mgoal, new_direction, mode, pointing_tasks)
-
+    status = {}
+    # Checking if task is already done
     if 'have_image' in state.__dict__.keys():
         if task in state.have_image.keys():
             if state.have_image[task]:
                 status['status'] = 'Done'
                 return status
-    
+
+    # Selecting the best satellite
+    status = select_best_satellite(state, mgoal, new_direction, mode, pointing_tasks)
+
+    # Failure case if no instruments can take the image.    
     if status['instrument'] == None:
         status['status'] = 'Fail'
 
+    # Instrument already calibrated hence just TakeImage.
     elif status['calibration_target'] == None:
         status['status'] = 'TakeImage'
 
@@ -142,8 +196,11 @@ def getImageStatus(state, mgoal, task, pointing_tasks):
         else:
             power_avail = True
         
+        # Power is available for calibration so Calibrate and TakeImage.
         if power_avail:
             status['status'] = 'Calib-and-TakeImage'
+
+        # No power is available hence switchOFF the instrument first.
         else:
             instrument = None
             for i, s in state.on_board.items():
@@ -160,7 +217,17 @@ def getImageStatus(state, mgoal, task, pointing_tasks):
 
 
 def getPointingStatus(state, mgoal, task):
+    """
+    Method to compute the status of pointing tasks.
 
+    Args:
+        state: The state of the system.
+        mgoal: The goal state.
+        task: The pointing task.
+
+    Returns:
+        Status of the task.
+    """
     status = 'Change-Pointing'
     
     if state.pointing[task] == mgoal.pointing[task]:
@@ -184,9 +251,19 @@ def m_achieveGoal(state, mgoal):
         Returns the plan if possible.
     """
 
+    # Initializing attributes that might not be given in the init state.
+    global first_run
+    if first_run:
+        first_run = False
+        state.power_on ={}
+        state.calibrated = {}
+        state.have_image = {}
+
+    # Get the tasks.
     pointing_tasks = getPointingTasks(state, mgoal)
     image_tasks = getHaveImageTasks(state, mgoal)
 
+    # Planning the image tasks.
     if image_tasks:
         for task in image_tasks:
             status = getImageStatus(state, mgoal, task, pointing_tasks)
@@ -210,6 +287,7 @@ def m_achieveGoal(state, mgoal):
             else:
                 continue
 
+    # Planning the pointing tasks.
     if pointing_tasks:
         for task in pointing_tasks:
             status = getPointingStatus(state, mgoal, task)
@@ -265,6 +343,7 @@ def m_calibrateInstrument(state, instrument, satellite):
     # Safety check before Switching On the instrument.
     if state.on_board[instrument] != satellite or not state.power_avail[satellite]:
         return False
+    
     plan.append(('switchOn', instrument, satellite))
 
     # Calibrating the instrument. 
@@ -280,7 +359,19 @@ gtpyhop.declare_task_methods('calibrateInstrument', m_calibrateInstrument)
 
 
 def m_storeImage(state, satellite, instrument, mode, new_direction):
-    
+    """
+    The method to store the Image of the requested location using the given mode.
+
+    Args:
+        state: The state of the system.
+        satellite: The satellite used for taking the image.
+        instrument: The instrument that will take the image.
+        mode: The mode of the instrument chosen.
+        new_direction: The direction in which we need the image.
+
+    Returns:
+        The plan that needs to be executed for storing the image.
+    """
     plan = list()
 
     # Change the pointing of the satellite if needed.
@@ -300,7 +391,17 @@ gtpyhop.declare_task_methods('storeImage', m_storeImage)
 
 
 def m_instrumentOff(state, instrument, satellite):
-    
+    """
+    The method to switch off an instrument.
+
+    Args:
+        state: The state of the system.
+        instrument: The instrument that will be turned OFF.
+        satellite: The satellite that houses the instrument.
+
+    Returns:
+        The plan that needs to be executed for switching off the instrument.
+    """
     if not state.power_on[instrument] and state.power_avail[satellite]:
         return []
     elif state.on_board[instrument] != satellite or not state.power_on[instrument]:
